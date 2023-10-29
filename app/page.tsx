f4150ca41a1be055330ca36a2107a4294d930fb4
steven <alvaradosteven315@gmail.com>
2023-10-28 22:57:29 -0500

    add global styles

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,8 @@ export default function Page() {
         {/* <AcmeLogo /> */}
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        <div className="flex flex-col justify-center gap-6 rounded-lg bg-slate-900 px-6 py-10 md:w-2/5 md:px-20">
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+        <div className="flex flex-col justify-center gap-6 rounded-lg bg-slate-200 px-6 py-10 md:w-2/5 md:px-20">
+          <p className={`text-2xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{" "}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
@@ -18,7 +18,7 @@ export default function Page() {
           </p>
           <Link
             href="/login"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base "
           >
             <span>Log in</span>
           </Link>
